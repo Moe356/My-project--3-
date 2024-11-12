3cc9a9609557468250dba6ae1dcaae1702c26f43
Muhammad Maraabah <badb28609@gmail.com>
2024-11-12 23:17:08 +0200

    Update lesson2.cs

--- Assets/scripts/lesson2.cs
+++ Assets/scripts/lesson2.cs
@@ -2,20 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lesson2 : MonoBehaviour
+public class MultiplicationTable : MonoBehaviour
 {
-    
-     int Multiply(int a, int b)
+    int Multiply(int a, int b)
     {
         return a * b;
     }
 
     void Start()
     {
+        int number = 5;
+
         for (int i = 1; i <= 10; i++)
         {
-            int result = Multiply(i, 5);
-            Debug.Log("5 x {i} = {result}");
+            int result = Multiply(number, i);
+            Debug.Log(number + " x " + i + " = " + result);
         }
     }
 }
